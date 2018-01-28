882e7a67cf2b309405d11459d16f41214c0e86fc
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-28 15:53:18 -0800

    implemented add/edit forms for react/redux + datepicker

---
+++ src/components/ExpenseListItem.js
@@ -0,0 +1,23 @@
+// Export a stateless functional component 
+// description, amount, createdAt
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { removeExpense } from '../actions/expenses';
+
+const ExpenseListItem = ({ dispatch, description, id, amount, createdAt }) => (
+	<div>
+
+		<Link to={`/edit/${id}`}>
+			<h3>{description}</h3>
+		</Link>
+
+		<p>{amount} - {createdAt}</p>
+		<button onClick={() => {
+			dispatch(removeExpense({ id }));
+		}}>Remove</button>
+		
+	</div>
+);
+
+export default connect()(ExpenseListItem);
