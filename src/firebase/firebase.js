233749dc6e9625196a6a958bd1384dc246fb89be
Tipper Truong <tqtruong95@Tippers-MacBook-Pro.local>
2018-01-29 19:31:04 -0800

    Added Firebase Authentication with Gmail

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -14,5 +14,6 @@ const config = {
 firebase.initializeApp(config);
 
 const database = firebase.database();
-export { firebase, database as default };
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
+export { firebase, googleAuthProvider, database as default };
--- src/index.js
+++ src/index.js
@@ -1,31 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import AppRouter from './routers/AppRouter';
+import AppRouter, { history } from './routers/AppRouter';
 import configureStore from './store/configureStore';
 import { startSetExpenses } from './actions/expenses';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { login, logout } from './actions/auth';
 import './styles/styles.scss';
-import './firebase/firebase';
+import { firebase } from './firebase/firebase';
 
 const store = configureStore();
-const jsx = (
-  <Provider store={store}>
-    <AppRouter />
-  </Provider>
-);
-let hasRendered = false;
-const renderApp = () => {
-  if (!hasRendered) {
-    ReactDOM.render(jsx, document.getElementById('#root'));
-    hasRendered = true;
-  }
-};
 
 ReactDOM.render(<p>Loading...</p>, document.querySelector('#root'));
 
 store.dispatch(startSetExpenses()).then(() => {
-	ReactDOM.render(jsx, document.querySelector('#root'));
+  ReactDOM.render(
+    <Provider store={store}>
+      <AppRouter />
+    </Provider>,
+    document.querySelector('#root')
+  );
+});
+
+let hasRendered = false;
+const renderApp = () => {
+  if (!hasRendered) {
+    ReactDOM.render(
+      <Provider store={store}>
+        <AppRouter />
+      </Provider>,
+      document.querySelector('#root')
+    );
+    hasRendered = true;
+  }
+};
+
+firebase.auth().onAuthStateChanged(user => {
+  if (user) {
+  	store.dispatch(login(user.uid));
+    store.dispatch(startSetExpenses()).then(() => {
+      renderApp();
+      if (history.location.pathname === '/') {
+        history.push('/dashboard');
+      }
+    });
+  } else {
+  	store.dispatch(logout());
+    renderApp();
+    history.push('/');
+  }
 });
